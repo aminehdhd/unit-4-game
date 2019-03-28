7343925741116935c988e70d2667a0e3a8aa03ac
Amine Haddouch <47290875+aminehdhd@users.noreply.github.com>
2019-03-27 22:58:06 -0700

    Add files via upload

---
+++ unit-4-game/game.js
@@ -0,0 +1,7 @@
+// var randomNumberField = document.getElementById('random-number')
+
+// var randomNumber = Math.floor(Math.random()* (120-19+1)+ 19)
+// randomNumberField.innerText = randomNumber
+function getweight(){
+    return "abc";
+}
